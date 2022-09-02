e2d474b0c352eef1c4321d03cf39daee1c8b6a89
Alex Teodorescu <56304311+alexsteo@users.noreply.github.com>
2022-09-02 18:53:06 +0300

    unicode suitrs

--- src/objects/game.js
+++ src/objects/game.js
@@ -1,6 +1,6 @@
 const {Card} = require("./card");
 
-const suits = ["H", "C", "D", "S"];
+const suits = ["♠", "♥", "♦", "♣"];
 const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 class Player {
